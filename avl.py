56feb4df3b003600f45f17fe3185e57964f2e4ce
bsilawko <mrufkazkrem@protonmail.com>
2024-04-14 10:20:16 +0200

    jakiś start

---
+++ avl.py
@@ -0,0 +1,10 @@
+class AVLNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.balance_factor: int = 0
+
+
+class AVLTree:
+    pass
---
+++ bst.py
@@ -0,0 +1,19 @@
+class BSTNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+class BSTree:
+    def insert(self, key) -> None:
+        pass
+
+    def search(self, key) -> BSTNode:
+        pass
+
+    def delete(self, key) -> None:
+        pass
+
+    def display(self):
+        pass
