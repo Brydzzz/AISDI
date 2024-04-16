621cd34eb5cd3081334278c326a3348a22d397e7
bsilawko <mrufkazkrem@protonmail.com>
2024-04-16 21:32:22 +0200

    bst search plot

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ from bst import BSTree
 from time import process_time
 import gc
 from matplotlib import pyplot as plt
-from random import randint
+from random import randint, choice
 
 
 def generate_random_list(size: int) -> list[int]:
@@ -13,18 +13,18 @@ def generate_random_list(size: int) -> list[int]:
 
 
 def gather_bst_insert_data(
-    nums_to_insert: list[int], step: int
+    keys_to_insert: list[int], step: int
 ) -> list[tuple[int, int]]:
-    length = len(nums_to_insert)
+    length = len(keys_to_insert)
     data = []
     for n in range(1, length, step):
-        nums = nums_to_insert[:n]
+        keys = keys_to_insert[:n]
         bst_tree = BSTree()
         gc_old = gc.isenabled()
         gc.disable()
         t1 = process_time()
-        for num in nums:
-            bst_tree.insert(num)
+        for key in keys:
+            bst_tree.insert(key)
         t2 = process_time()
         if gc_old:
             gc.enable()
@@ -44,22 +44,59 @@ def create_bst_insert_plot(data: list[tuple[int, int]]) -> None:
     plt.clf()
 
 
-def main():
-    nums = generate_random_list(400000)
+def gather_bst_search_data(
+    keys_in_bst: list[int], step: int, key_to_search: int
+) -> list[tuple[int, int]]:
+    length = len(keys_in_bst)
+    data = []
+    for n in range(1, length, step):
+        keys = keys_in_bst[:n]
+        bst_tree = BSTree(list=keys)
+        gc_old = gc.isenabled()
+        gc.disable()
+        t1 = process_time()
+        bst_tree.search(key_to_search)
+        t2 = process_time()
+        if gc_old:
+            gc.enable()
+        data.append((n, t2 - t1))
+    return data
 
-    # gather BST insert data
-    print("Started gathering data for BSTree insert()")
-    bst_insert_data = gather_bst_insert_data(nums, 10000)
-    print("Finished gathering data for BSTree insert()")
+
+def create_bst_search_plot(data: list[tuple[int, int]]) -> None:
+    size_values = [point[0] for point in data]
+    time_values = [point[1] for point in data]
+    plt.plot(size_values, time_values, marker="o", linestyle="-")
+    plt.title("BSTree Search")
+    plt.ylabel("Time [s]")
+    plt.ylim(top=10 ** (-4))
+    plt.xlabel("Number of n elements")
+    plt.grid()
+    plt.savefig("bst_search_plot.png")
+    plt.clf()
+
+
+def main():
+    keys = generate_random_list(40000)
+    key_to_search = choice(keys)
+
+    # # gather BST insert data
+    # print("Started gathering data for BSTree insert()")
+    # bst_insert_data = gather_bst_insert_data(keys, 10000)
+    # print("Finished gathering data for BSTree insert()")
+    # # plot BST insert data
+    # create_bst_insert_plot(bst_insert_data)
 
     # gather BST search data
+    print("Started gathering data for BSTree search()")
+    bst_search_data = gather_bst_search_data(keys, 5000, key_to_search)
+    print("Finished gathering data for BSTree search()")
+    # plot BST search data
+    create_bst_search_plot(bst_search_data)
+
     # gather BST delete data
 
-    # plot BST insert data
-    create_bst_insert_plot(bst_insert_data)
-    # plot BST search data
     # plot BST delete data
-    pass
 
 
 if __name__ == "__main__":
