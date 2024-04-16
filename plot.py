0d70f9043e3ad45bec4babb578d23edb5df5a4d1
bsilawko <mrufkazkrem@protonmail.com>
2024-04-16 22:27:36 +0200

    bst delete

--- plot.py
+++ plot.py
@@ -76,16 +76,51 @@ def create_bst_search_plot(data: list[tuple[int, int]]) -> None:
     plt.clf()
 
 
+def gather_bst_delete_data(
+    keys_in_bst: list[int], step: int, key_to_delete: int
+) -> list[tuple[int, int]]:
+    length = len(keys_in_bst)
+    data = []
+    for n in range(1, length, step):
+        keys = keys_in_bst[:n]
+        bst_tree = BSTree(list=keys)
+        gc_old = gc.isenabled()
+        gc.disable()
+        t1 = process_time()
+        bst_tree.delete(key_to_delete)
+        t2 = process_time()
+        if gc_old:
+            gc.enable()
+        data.append((n, t2 - t1))
+    return data
+
+
+def create_bst_delete_plot(data: list[tuple[int, int]]) -> None:
+    size_values = [point[0] for point in data]
+    time_values = [point[1] for point in data]
+    plt.plot(size_values, time_values, marker="o", linestyle="-")
+    plt.title("BSTree Delete")
+    plt.ylabel("Time [ms]")
+    plt.ylim(bottom=0, top=10 ** (-4))
+    plt.xlim(left=0)
+    plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
+    plt.xlabel("Number of n elements")
+    plt.grid()
+    plt.savefig("bst_delete_plot.png")
+    plt.clf()
+
+
 def main():
     keys = generate_random_list(40000)
     key_to_search = choice(keys)
+    key_to_delete = choice(keys)
 
-    # # gather BST insert data
-    # print("Started gathering data for BSTree insert()")
-    # bst_insert_data = gather_bst_insert_data(keys, 10000)
-    # print("Finished gathering data for BSTree insert()")
-    # # plot BST insert data
-    # create_bst_insert_plot(bst_insert_data)
+    # gather BST insert data
+    print("Started gathering data for BSTree insert()")
+    bst_insert_data = gather_bst_insert_data(keys, 10000)
+    print("Finished gathering data for BSTree insert()")
+    # plot BST insert data
+    create_bst_insert_plot(bst_insert_data)
 
     # gather BST search data
     print("Started gathering data for BSTree search()")
@@ -95,8 +130,11 @@ def main():
     create_bst_search_plot(bst_search_data)
 
     # gather BST delete data
-
+    print("Started gathering data for BSTree delete()")
+    bst_delete_data = gather_bst_search_data(keys, 1000, key_to_delete)
+    print("Finished gathering data for BSTree delete()")
     # plot BST delete data
+    create_bst_delete_plot(bst_delete_data)
 
 
 if __name__ == "__main__":
