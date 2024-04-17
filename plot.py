3ef4fddd2ddb4f352dbb0b239eaba94f3c5378e7
bsilawko <mrufkazkrem@protonmail.com>
2024-04-17 15:01:34 +0200

    added magic consts

--- plot.py
+++ plot.py
@@ -18,7 +18,7 @@ def gather_bst_insert_data(
 ) -> list[tuple[int, int]]:
     length = len(keys_to_insert)
     data = []
-    for n in range(1, length, step):
+    for n in range(0, length, step):
         keys = keys_to_insert[:n]
         bst_tree = BSTree()
         gc_old = gc.isenabled()
@@ -56,7 +56,7 @@ def gather_bst_search_data(
 ) -> list[tuple[int, int]]:
     length = len(keys_in_bst)
     data = []
-    for n in range(1, length, step):
+    for n in range(0, length, step):
         keys = keys_in_bst[:n]
         bst_tree = BSTree(list=keys)
         gc_old = gc.isenabled()
@@ -96,7 +96,7 @@ def gather_bst_delete_data(
 ) -> list[tuple[int, int]]:
     length = len(keys_in_bst)
     data = []
-    for n in range(1, length, step):
+    for n in range(0, length, step):
         keys = keys_in_bst[:n]
         bst_tree = BSTree(list=keys)
         gc_old = gc.isenabled()
@@ -115,7 +115,7 @@ def create_bst_delete_plot(data: list[tuple[int, int]]) -> None:
     time_values = [point[1] for point in data]
     plt.plot(size_values, time_values, marker="o", linestyle="-")
     plt.title("BSTree Delete")
-    plt.ylabel("Time [ms]")
+    plt.ylabel("Time [s]")
     plt.ylim(bottom=0, top=10 ** (-4))
     plt.xlim(left=0)
     plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
@@ -246,41 +246,43 @@ def main():
     keys = generate_random_list(40001)
     key_to_search = choice(keys)
     key_to_delete = choice(keys)
-    keys_sorted = sorted(keys[:20001])
+    keys_sorted = sorted(keys[:15001])
+    SORTED_STEP = 1500
+    RANDOM_STEP = 1000
 
     # gather BST insert data
     print("Started gathering data for BSTree insert()")
-    bst_insert_random_data = gather_bst_insert_data(keys, 1000)
-    bst_insert_sorted_data = gather_bst_insert_data(keys_sorted, 5000)
+    bst_insert_random_data = gather_bst_insert_data(keys, RANDOM_STEP)
+    bst_insert_sorted_data = gather_bst_insert_data(keys_sorted, SORTED_STEP)
     print("Finished gathering data for BSTree insert()")
     # plot BST insert data
     create_bst_insert_plot(bst_insert_random_data)
 
     # gather BST search data
     print("Started gathering data for BSTree search()")
-    bst_search_data = gather_bst_search_data(keys, 1000, key_to_search)
+    bst_search_data = gather_bst_search_data(keys, RANDOM_STEP, key_to_search)
     print("Finished gathering data for BSTree search()")
     # plot BST search data
     create_bst_search_plot(bst_search_data)
 
     # gather BST delete data
     print("Started gathering data for BSTree delete()")
-    bst_delete_data = gather_bst_search_data(keys, 1000, key_to_delete)
+    bst_delete_data = gather_bst_search_data(keys, RANDOM_STEP, key_to_delete)
     print("Finished gathering data for BSTree delete()")
     # plot BST delete data
     create_bst_delete_plot(bst_delete_data)
 
     # gather AVLTree insert data
     print("Started gathering data for AVLTree insert()")
-    avl_insert_random_data = gather_avl_insert_data(keys, 1000)
-    avl_insert_sorted_data = gather_avl_insert_data(keys_sorted, 5000)
+    avl_insert_random_data = gather_avl_insert_data(keys, RANDOM_STEP)
+    avl_insert_sorted_data = gather_avl_insert_data(keys_sorted, SORTED_STEP)
     print("Finished gathering data for AVLTree insert()")
     # plot AVLTree insert data
     create_avl_insert_plot(avl_insert_random_data)
 
     # gather AVLTree search data
     print("Started gathering data for AVLTree search()")
-    avl_search_data = gather_avl_search_data(keys, 1000, key_to_search)
+    avl_search_data = gather_avl_search_data(keys, RANDOM_STEP, key_to_search)
     print("Finished gathering data for AVLTree search()")
     # plot AVLTree search data
     create_avl_search_plot(avl_search_data)
