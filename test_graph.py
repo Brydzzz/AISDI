5ce84ae3973f1c893a6742f7f4931ffaeb058714
Tomasz <tom.p.smolen@gmail.com>
2024-04-26 00:49:31 +0200

    added tests for lt operator

--- test_graph.py
+++ test_graph.py
@@ -1,1 +1,19 @@
 # TODO write tests for Vertice and Graph classes
+from graph import Vertice
+
+
+def test_vertice_lt():
+    a = Vertice("a")
+    a.distance = 10
+    b = Vertice("b")
+    b.distance = 20
+    assert a < b
+    assert not b < a
+
+
+def test_vertice_max_distancelt():
+    a = Vertice("a")
+    a.distance = 10
+    b = Vertice("b")
+    assert a < b
+    assert not b < a
