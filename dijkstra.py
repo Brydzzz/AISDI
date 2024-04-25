33b33e0a99062adb4a8da81fa3f74a596f9b72c2
Tomasz <tom.p.smolen@gmail.com>
2024-04-26 00:48:46 +0200

    implemented dijkstra algorithm

---
+++ dijkstra.py
@@ -0,0 +1,29 @@
+from heapq import heappush, heappop
+
+
+def dijkstra(graph, start, end) -> None:
+    queue = [start]
+    start.distance = 0
+    while queue:
+        source = queue[0]
+        if source == end:
+            break
+        if source.checked is False:
+            edges = graph.adj_list[source]
+            for edge in edges:
+                dest = edge[0]
+                weight = edge[1]
+                if dest.distance:
+                    if weight + source.distance < dest.distance:
+                        dest.parent = source
+                        dest.distance = weight + source.distance
+                        heappush(queue, dest)
+                    # else:
+                    #     dest.parent = source
+                    #     dest.distance = weight + source.distance
+                    #     heappush(queue, dest)
+            heappop(queue)
+            source.checked = True
+        else:
+            heappop(queue)
+    return
