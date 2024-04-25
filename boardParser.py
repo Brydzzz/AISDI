b827eccf06966ad7f6d3c563b93d948fb32ff7c9
Tomasz <tom.p.smolen@gmail.com>
2024-04-25 23:52:39 +0200

    implemented has_x methods + get_weight

--- boardParser.py
+++ boardParser.py
@@ -15,24 +15,29 @@ class BoardParser:
         return board
 
     def has_above(self, src_row: int, src_col: int) -> bool:
-        # TODO implement
+        if src_row == 0:
+            return False
         return True
 
     def has_under(self, src_row: int, src_col: int) -> bool:
-        # TODO implement
+        if src_row + 1 == len(self.board):
+            return False
         return True
 
     def has_left(self, src_row: int, src_col: int) -> bool:
-        # TODO implement
+        if src_col == 0:
+            return False
         return True
 
     def has_right(self, src_row: int, src_col: int) -> bool:
-        # TODO implement
+        if src_col + 1 == len(self.board[src_row]):
+            return False
         return True
 
     def get_weight(self, vertice: Vertice) -> int:
-        # TODO implement
-        pass
+        if vertice.key == "J" or vertice.key == "X":
+            return 0
+        return int(vertice.key)
 
     def create_graph(self) -> Graph:
         graph = Graph()
@@ -40,29 +45,43 @@ class BoardParser:
             for col_idx, element in enumerate(row):
                 graph.add_vertice(element)
 
-                # TODO add checks for J
-
                 # above edge
                 if self.has_above(row_idx, col_idx):
-                    above_element = self.board[row_idx - 1, col_idx]
-                    weight = self.get_weight(above_element)
+                    above_element = self.board[row_idx - 1][col_idx]
+                    weight = (
+                        0
+                        if element.key == "J"
+                        else self.get_weight(above_element)
+                    )
                     graph.add_edge(element, above_element, weight)
 
                 # under edge
                 if self.has_under(row_idx, col_idx):
-                    under_element = self.board[row_idx + 1, col_idx]
-                    weight = self.get_weight(under_element)
+                    under_element = self.board[row_idx + 1][col_idx]
+                    weight = (
+                        0
+                        if element.key == "J"
+                        else self.get_weight(under_element)
+                    )
                     graph.add_edge(element, under_element, weight)
 
                 # left edge
                 if self.has_left(row_idx, col_idx):
-                    left_element = self.board[row_idx, col_idx - 1]
-                    weight = self.get_weight(left_element)
+                    left_element = self.board[row_idx][col_idx - 1]
+                    weight = (
+                        0
+                        if element.key == "J"
+                        else self.get_weight(left_element)
+                    )
                     graph.add_edge(element, left_element, weight)
 
                 # right edge
                 if self.has_right(row_idx, col_idx):
-                    right_element = self.board[row_idx, col_idx + 1]
-                    weight = self.get_weight(right_element)
+                    right_element = self.board[row_idx][col_idx + 1]
+                    weight = (
+                        0
+                        if element.key == "J"
+                        else self.get_weight(right_element)
+                    )
                     graph.add_edge(element, right_element, weight)
         return graph
