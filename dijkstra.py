624d268ce50fc60c522e49776600b65b3d40f401
Tomasz <tom.p.smolen@gmail.com>
2024-05-06 21:02:14 +0200

    refactoring

--- dijkstra.py
+++ dijkstra.py
@@ -13,21 +13,18 @@ def dijkstra(graph, start, end) -> None:
             for edge in edges:
                 dest = edge[0]
                 weight = edge[1]
-                if dest.distance:
-                    if weight + source.distance < dest.distance:
-                        dest.parent = source
-                        dest.distance = weight + source.distance
-                        heappush(queue, dest)
+                # if dest.distance:
+                if weight + source.distance < dest.distance:
+                    dest.parent = source
+                    dest.distance = weight + source.distance
+                    heappush(queue, dest)
                     # else:
                     #     dest.parent = source
                     #     dest.distance = weight + source.distance
                     #     heappush(queue, dest)
-            heappop(queue)
             source.checked = True
-        else:
-            heappop(queue)
+        heappop(queue)
     mark_vertices(start, end)
-    return
 
 
 def mark_vertices(start, end) -> None:
