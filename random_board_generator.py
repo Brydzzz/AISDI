5732fef3d199aa5d77c1cb311f5f5c6441f56c70
bsilawko <mrufkazkrem@protonmail.com>
2024-05-06 21:32:26 +0200

    added argparser

--- random_board_generator.py
+++ random_board_generator.py
@@ -1,13 +1,12 @@
 from random import choice, randint
+import argparse
 
 
-def generate_random_board(x, y) -> None:
+def generate_random_board(x, y, filename) -> None:
     list = ["J", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    # title = f"random_board{x}x{y}.txt"
-    with open("random_board.txt", mode="w") as file_handler:
+    with open(filename, mode="w") as file_handler:
         x1_pos = (randint(1, x - 1), randint(1, y - 1))
         x2_pos = (randint(1, x - 1), randint(1, y - 1))
-        print(x1_pos, x2_pos)
         row_number = 0
         while row_number != y:
             line = ""
@@ -25,4 +24,18 @@ def generate_random_board(x, y) -> None:
 
 
 if __name__ == "__main__":
-    generate_random_board(20, 20)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "x", metavar="number_of_rows", help="Number of board rows", type=int
+    )
+    parser.add_argument(
+        "y",
+        metavar="number_of_columns",
+        help="Number of board columns",
+        type=int,
+    )
+    parser.add_argument(
+        "output", metavar="output_file", help="file where board will be saved"
+    )
+    args = parser.parse_args()
+    generate_random_board(args.x, args.y, args.output)
