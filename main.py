dbf4771bd0108bad1be51db895e4a424dc048dd6
Tomasz <tom.p.smolen@gmail.com>
2024-05-06 21:16:50 +0200

    added output file arg

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dijkstra import dijkstra
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="file with board")
+    parser.add_argument("output_file", help="route output file")
     args = parser.parse_args()
     filename = args.filename
     with open(filename, "r") as file:
@@ -14,7 +15,7 @@ def main():
     x1, x2 = b.find_start_end()
     dijkstra(gr, x1, x2)
     b.print_route(x2)
-    b.save_route("route.txt", x2)
+    b.save_route(args.output_file, x2)
 
 
 if __name__ == "__main__":
